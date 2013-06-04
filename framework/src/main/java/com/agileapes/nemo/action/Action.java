85df697d0949427b2c73fd9d60fc7e788747e30f
Milad Naseri <m.m.naseri@gmail.com>
2013-06-04 16:36:33 +0430

    License and default structure

---
+++ framework/src/main/java/com/agileapes/nemo/action/Action.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2013. AgileApes (http://www.agileapes.scom/), and
+ * associated organization.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ */
+
+package com.agileapes.nemo.action;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (2013/6/4, 16:35)
+ */
+public class Action {
+
+
+
+}
